ec1770a1372df9d9ef3215263d96da241f5ebc16
mudit9 <msgagent1@gmail.com>
2019-02-15 15:23:20 +0530

    a

--- app.py
+++ app.py
@@ -40,9 +40,6 @@ def predict_stuff_user():
         n3 = request.form.get('id3')
         n4 = request.form.get('id4')
         n5 = request.form.get('id5')
-        print n1
-        print n2
-        print n3
         rating1 = request.form.get('rating1')
         user_rating = user_rating.append({'UserID': 1000, 'MovieID': int(n1), 'Rated': rating1}, ignore_index=True)
         rating2 = request.form.get('rating2')
@@ -54,23 +51,28 @@ def predict_stuff_user():
         rating5 = request.form.get('rating5')
         user_rating = user_rating.append({'UserID': 1000, 'MovieID': int(n5), 'Rated': rating5}, ignore_index=True)
         #ITEM ITEM
-        max = user_rating.loc[user_rating['UserID'] == 1000, ['Rated']]
-        max2 = max['Rated'].astype(float).idxmax()
-        max_movie_id = user_rating.iloc[max2]['MovieID']
-        max_movie_name1 = movies_data.loc[movies_data['MovieID'] == max_movie_id, ['Name']]
-        print ("afa")
-        print max_movie_name1
-        max_movie_name = max_movie_name1.iloc[0]['Name']
-        movie_data = pd.merge(movies_data,user_rating, on='MovieID')
-        user_movie_rating = movie_data.pivot_table(index='UserID', columns='Name', values='Rated',aggfunc='first').astype(float)
-        ratings = user_movie_rating[str(max_movie_name)].astype(float)
-        movies_like = user_movie_rating.corrwith(ratings,method='pearson')
-        corr= pd.DataFrame(movies_like, columns=['Correlation'])
-        corr.dropna(inplace=True)
-        corr = corr.sort_values('Correlation', ascending=False).head(6)
-        recommends = list(corr.index)
-        recommends.pop(0)
-        return render_template("recommend.html",title = "Item-Item",movie_name = max_movie_name,rec1 = recommends[0],rec2 = recommends[1],rec3 = recommends[2],rec4 = recommends[3],rec5 = recommends[4])
+        users_rating= user_rating.loc[user_rating['UserID'] == 1000, ['Rated']] #get the new users rating
+        max2 = users_rating['Rated'].astype(float).idxmax() #find the index of the maximum rated movie
+        print (user_rating.iloc[max2]['Rated'])
+        if int(user_rating.iloc[max2]['Rated'])>3: #See if user has given any of the movie more than 3 stars
+            print ("GOING HERE")
+            max_movie_id = user_rating.iloc[max2]['MovieID'] #Get the movie ID from the index
+            max_movie_name1 = movies_data.loc[movies_data['MovieID'] == max_movie_id, ['Name']] #Get the name of the movie from ID
+            max_movie_name = max_movie_name1.iloc[0]['Name']
+            movie_data = pd.merge(movies_data,user_rating, on='MovieID') #merge the 2DFs
+            user_movie_rating=movie_data.pivot_table(index='UserID',columns='Name',values='Rated',aggfunc='first').astype(float)#utility Matrix
+            ratings = user_movie_rating[str(max_movie_name)].astype(float)#Get the ratings of the max rated movie
+            movie_like=user_movie_rating.corrwith(ratings,method='pearson')#Find the movies with similar ratings(pearsonSimilarity)
+            corr= pd.DataFrame(movie_like, columns=['Correlation'])
+            corr.dropna(inplace=True)
+            corr = corr.sort_values('Correlation',ascending=False).head(6) #Sort it in a descending order. Maximum first.
+            recommends = list(corr.index)
+            recommends.pop(0)
+            return render_template("recommend.html",title = "Item-Item",movie_name = max_movie_name,rec1 = recommends[0],rec2 = recommends[1],rec3 = recommends[2],rec4 = recommends[3],rec5 = recommends[4])
+        else:
+            recommends = list(movies_data.sort_values("Rating",ascending=False).head(5)['Name'])
+            return render_template("recommend.html",title = "Item-Item",no_movie_name = "a",rec1 = recommends[0],rec2 = recommends[1],rec3 = recommends[2],rec4 = recommends[3],rec5 = recommends[4])
+
 
 @app.route('/get-user-data-user', methods=['POST'])
 def predict_stuff_item():
@@ -89,19 +91,27 @@ def predict_stuff_item():
         rating5 = request.form.get('rating5')
         user_rating = user_rating.append({'UserID': 1000, 'MovieID': movieids[4], 'Rated': rating5}, ignore_index=True)
                 #USER USER1
-        movie_data = pd.merge(movies_data,user_rating, on='MovieID')
-        movie_user_rating = movie_data.pivot_table(index= 'Name',columns = 'UserID',values='Rated',aggfunc= 'first').astype(float)
-        user_rating = movie_user_rating[1000].astype(float)
-        user_like_user = movie_user_rating.corrwith(user_rating,method='pearson')
-        corr_user = pd.DataFrame(user_like_user, columns=['Correlation'])
+        movie_data = pd.merge(movies_data,user_rating, on='MovieID') #merge the 2DFs
+        movie_user_rating=(movie_data.pivot_table(index= 'Name',columns='UserID',values='Rated',aggfunc='first')
+                   .astype(float).fillna(0))#UtlityMatrix
+        usern_rating = movie_user_rating[1000].astype(float) #Get ratings of new user(index = 1000)
+        user_alike=movie_user_rating.corrwith(usern_rating,method='pearson')#find user with similar ratings(pearson Similarity)
+        corr_user = pd.DataFrame(user_alike, columns=['Correlation'])
         corr_user.dropna(inplace=True)
-        corr_user = corr_user.sort_values('Correlation', ascending=False).head(10)
-        similar_user = list(corr_user.index)[1]
-        similar_user_rating = movie_user_rating[similar_user]
-        user_rating.dropna(inplace=True)
+        corr_user = corr_user.sort_values('Correlation',ascending=False).head(10)#Descending order to get maximum ratings first
+        similar_user = list(corr_user.index)[1] #Most similar user
+        similar_user_rating = movie_user_rating[similar_user] #Get Ratings of the most similar user
+        usern_rating.dropna(inplace=True)
         similar_user_rating.dropna(inplace=True)
-        similar_user_rating = similar_user_rating.sort_values(ascending=False).head(6)
+        similar_user_rating = similar_user_rating.sort_values(ascending=False).head(6)#Highest rated movies of similar user
+        user_data = user_rating[user_rating.UserID == (1000)]
+        user_full = (user_data.merge(movies_data, how = 'left', left_on = 'MovieID', right_on = 'MovieID')
+                     .sort_values(['Rated'], ascending=False)) #find movies watched by new user
+        movies_watched = list(user_full['MovieID'])
         recommended_movies = list(similar_user_rating.index)
+        for movie in recommended_movies:              #check if any recommended movie has already been rated by new user
+            if movie in movies_watched:
+                recommended_movies.pop(movie)         #remove from recommendations if already rated
         recommended_movies.pop(0)
         return render_template("recommend.html",similar_user = similar_user,rating_1=similar_user_rating[0],rating_2=similar_user_rating[1],rating_3=similar_user_rating[2],rating_4=similar_user_rating[3],rating_5=similar_user_rating[5],title = "User-User",rec1 = recommended_movies[0],rec2 =recommended_movies[1],rec3 = recommended_movies[2],rec4 = recommended_movies[3],rec5 = recommended_movies[4])
 
@@ -124,30 +134,23 @@ def predit_stuff_matrix():
         movie_data = pd.merge(movies_data,user_rating, on='MovieID')
         user_movie_rating = movie_data.pivot_table(index='UserID', columns='Name', values='Rated',aggfunc='first')
         # MATRIX decomposition
-        matrix_df = user_rating.pivot_table(index = 'UserID',columns ='MovieID', values = 'Rated',aggfunc='first').astype(float).fillna(0)
+        matrix_df = user_rating.pivot_table(index='UserID',columns ='MovieID',values='Rated',aggfunc='first').astype(float).fillna(0)  #utility matrix
         R = matrix_df.as_matrix()
-        user_ratings_mean = np.mean(R, axis = 1)
+        user_ratings_mean = np.mean(R, axis = 1)        #Demeaning the data
         matrix_de_mean = R - user_ratings_mean.reshape(-1, 1)
-        U, sigma, Vt = svds(matrix_de_mean, k = 50)
-        sigma = np.diag(sigma)
-        all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
-        prediction_df = pd.DataFrame(all_user_predicted_ratings, columns = user_movie_rating.columns)
-        def recommend_movies(predictions_df, userID, movies_df, users_ratings_df, num_recommendations=5):
-
+        U, sigma, Vt = svds(matrix_de_mean, k = 50)    #Singular Value Decomposition
+        sigma = np.diag(sigma)                         #Converting into a diagonal matrix
+        all_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1) #Getting original data
+        prediction_df = pd.DataFrame(all_user_predicted_ratings, columns = user_movie_rating.columns) #predictions DF
+        def recommend_movies(predictions_df, userID, movies_df, users_ratings_df):
             user_number = userID - 1
             sorted_predictions = predictions_df.iloc[user_number].sort_values(ascending=False)
-
-            user_data = users_ratings_df[users_ratings_df.UserID == (userID)]
-            user_full = (user_data.merge(movies_df, how = 'left', left_on = 'MovieID', right_on = 'MovieID').
-                             sort_values(['Rated'], ascending=False))
-
             recommendations = (movies_df[~movies_df['MovieID'].isin(user_full['MovieID'])].
-                 merge(pd.DataFrame(sorted_predictions).reset_index(), how = 'left').
-                 rename(columns = {user_number: 'Predictions'}).
-                 sort_values('Predictions', ascending = False).iloc[:num_recommendations, :-1])
-            return user_full, recommendations
-
-        already_rated, predictions = recommend_movies(prediction_df, 1000, movies_data,user_rating, 20)
+                         merge(pd.DataFrame(sorted_predictions).reset_index(), how = 'left').
+                         rename(columns = {user_number: 'Predictions'}).
+                         sort_values('Predictions', ascending = False).iloc[:5, :-1])
+            return recommendations
+        predictions = recommend_movies(prediction_df, 1000, movies_data,user_rating)
         recommended_movies = list(predictions['Name'].head(5))
         return render_template("recommend.html",title = "Matrix decomposition",rec1 = recommended_movies[0],rec2 =recommended_movies[1],rec3 = recommended_movies[2],rec4 = recommended_movies[3],rec5 = recommended_movies[4])
 
